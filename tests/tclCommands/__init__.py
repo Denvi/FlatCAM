56ba233fd66c3b56112b6c7af4d7cb696cef0a02
sopak <kamil.sopko@jcode.cz>
2016-04-24 22:44:28 +0200

    implement test_TclCommandExteriors implement test_TclCommandInteriors

--- tests/tclCommands/__init__.py
+++ tests/tclCommands/__init__.py
@@ -7,9 +7,9 @@ from test_TclCommandAddPolyline import *
 # from test_TclCommandCncjob import *
 # from test_TclCommandDrillcncjob import *
 # from test_TclCommandExportGcode import *
-# from test_TclCommandExteriors import *
+from test_TclCommandExteriors import *
 from test_TclCommandImportSvg import *
-# from test_TclCommandInteriors import *
+from test_TclCommandInteriors import *
 from test_TclCommandIsolate import *
 from test_TclCommandNew import *
 from test_TclCommandNewGeometry  import *
